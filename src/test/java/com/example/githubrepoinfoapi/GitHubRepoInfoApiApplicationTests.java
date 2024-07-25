eb484820a53c6fba72d36586eba34f0ef390b598
Basia <barbarawojciechowskaa@gmail.com>
2024-07-25 21:39:40 +0200

    init

---
+++ src/test/java/com/example/githubrepoinfoapi/GitHubRepoInfoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.githubrepoinfoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitHubRepoInfoApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
